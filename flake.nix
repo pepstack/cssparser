04aeffe47b1c6b4343e739f5774bcba2fc3632b9
Tristan Ross <tross@ezoic.com>
2022-09-23 15:51:15 -0700

    feat: added nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A pure-C CSS parser forked from katana-parser.";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "i686-linux"
+        "riscv64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            name = "cssparser";
+            src = self;
+
+            outputs = [ "out" "dev" ];
+            enableParallelBuilding = true;
+            nativeBuildInputs = with pkgs; [ autoconf automake libtool gnumake autoreconfHook ];
+
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/pepstack/cssparser";
+              license = with licenses; [ mit ];
+              maintainers = [ "Tristan Ross" "pepstack" ];
+            };
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ autoconf automake libtool gnumake autoreconfHook ];
+          };
+        });
+    };
+}
